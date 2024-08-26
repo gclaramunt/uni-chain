01b7ecb40be9285ca2db19512388f3623fccc53c
Gabriel Claramunt <gabriel.claramunt@gmail.com>
2024-08-26 00:04:27 -0300

    refactor for db

--- src/main/scala/gclaramunt/unichain/UnichainService.scala
+++ src/main/scala/gclaramunt/unichain/UnichainService.scala
@@ -1,10 +1,15 @@
 package gclaramunt.unichain
 
-import cats.effect.Ref
+import cats.effect.kernel.Concurrent
+import cats.effect.{Async, Ref}
+import cats.syntax.all.*
+import doobie.Transactor
+import doobie.hikari.HikariTransactor.fromHikariConfig
 import gclaramunt.unichain.blockchain.CryptoTypes.Address
 import gclaramunt.unichain.blockchain.{Block, Transaction}
+import gclaramunt.unichain.store.{LedgerDB, hikariConfig}
 
-class UnichainService[F[_]](lastBlock: Ref[F, Block], ledgerStatus: Ref[F,Map[Address, BigDecimal]]):
+class UnichainService[F[_]](lastBlock: Ref[F, Option[Block]], ledgerStatus: Ref[F,Map[Address, BigDecimal]]):
   
   def submitTx(tx: Transaction): F[String] = ???
 
@@ -15,6 +20,23 @@ class UnichainService[F[_]](lastBlock: Ref[F, Block], ledgerStatus: Ref[F,Map[Ad
     
   //val ledgerStatus: Map[Address, BigDecimal]
 
+object UnichainService {
 
+  def apply[F[_]: Async](xa: Transactor[F]): F[UnichainService[F]] = {
+    val ledgerDb = LedgerDB(xa)
+    for {
+//      xa <- fromHikariConfig[F](hikariConfig)
+      lastBlock <- ledgerDb.getLastBlock()
+      ledger <- buildLedger(ledgerDb.getTransactions())
+      blockRef <- Ref.of(lastBlock)
+      ledgerRef <- Ref.of(ledger)
+    } yield new UnichainService[F](blockRef,ledgerRef)
+  }
 
-
+  def buildLedger[F[_]: Concurrent](txs: fs2.Stream[F, Transaction]): F[Map[Address, BigDecimal]] =
+    txs.compile.fold(Map.empty[Address, BigDecimal]){case (m, tx) =>
+      val k = tx.destination
+      val currentVal = m.getOrElse(tx.destination, BigDecimal(0))
+      m + (tx.destination -> (currentVal + tx.amount))
+    }
+}
