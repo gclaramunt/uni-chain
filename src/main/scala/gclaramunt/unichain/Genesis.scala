3e8af87563f2c6b5e90a17d3a3821a4a0ab4926d
Gabriel Claramunt <gabriel.claramunt@gmail.com>
2024-08-27 23:45:14 -0300

    create db on genesis

--- src/main/scala/gclaramunt/unichain/Genesis.scala
+++ src/main/scala/gclaramunt/unichain/Genesis.scala
@@ -2,12 +2,15 @@ package gclaramunt.unichain
 
 import cats.effect.{IO, IOApp}
 import doobie.hikari.HikariTransactor.fromHikariConfig
+import doobie.implicits.toSqlInterpolator
 import gclaramunt.unichain.Config.nodeConfig
 import gclaramunt.unichain.blockchain.BlockchainOps.{blockHash, buildTx}
 import gclaramunt.unichain.blockchain.CryptoOps.{hash, pubKeyToAddress, sign}
 import gclaramunt.unichain.blockchain.CryptoTypes.Hash
 import gclaramunt.unichain.blockchain.{Block, BlockchainOps, Transaction}
 import gclaramunt.unichain.store.LedgerDB
+import doobie._
+import doobie.implicits._
 
 object Genesis extends IOApp.Simple:
 
@@ -26,7 +29,28 @@ object Genesis extends IOApp.Simple:
     fromHikariConfig[IO](Config.hikariConfig).use { xa =>
         val ledgerDb = LedgerDB(xa)
         for {
+          _ <- Schema.blockTable.transact(xa)
+          _ <- Schema.txTable.transact(xa)
           _ <- ledgerDb.addBlock(genesisBlock)
           _ <- ledgerDb.addTransaction(newId, treasuryTx)
         } yield ()
       }
+
+
+object Schema:
+
+  val blockTable: doobie.ConnectionIO[Int] =sql"""CREATE TABLE IF NOT EXISTS blocks(
+    id BIGINT,
+    previous_hash VARBINARY,
+    signature VARBINARY
+  );""".update.run
+
+  val txTable: doobie.ConnectionIO[Int] = sql"""CREATE TABLE IF NOT EXISTS transactions(
+    source VARCHAR,
+    destination VARCHAR,
+    amount NUMERIC,
+    signature VARBINARY,
+    hash VARBINARY,
+    nonce BIGINT,
+    block_id BIGINT
+  );""".update.run
